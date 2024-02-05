ca59ca740eecc0e04f9a7c8eb8271738af48a9f9
xezernn <xezernovruz79@gmail.com>
2024-02-05 12:28:28 +0400

    slider tours

---
+++ Js/audio.js
@@ -0,0 +1,17 @@
+// const ses = document.getElementById("ses");
+// const airplane = document.querySelector(".airplane");
+
+// function go() {
+//     const computedStyle = window.getComputedStyle(airplane);
+//     const topValue = parseInt(computedStyle.top, 10);
+
+//     console.log(topValue);
+
+//     if (topValue > 455 && topValue < 460) {
+//         ses.play();
+//     }
+
+//     requestAnimationFrame(go);
+// }
+
+// go();
--- Js/swiper.js
+++ Js/swiper.js
@@ -1,16 +1,35 @@
-var swiper = new Swiper(".mySwiper", {
+// const swiper = new Swiper("header .mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         // type: "fraction",    
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+
+/* Main birinci slider (tuors) */
+
+const swiper2 = new Swiper(".tours .mySwiper", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1100: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      }
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+  });
